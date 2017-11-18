eb807724f71d842bc8661207a1162ebcc09bf2ac
Loni mitchell <loni.mitchell@mymona.uwi.edu>
2017-11-18 03:53:30 +0000

    lab7

---
+++ world.php
@@ -0,0 +1,31 @@
+<?php
+$host = getenv('IP');
+$username = getenv('C9_USER');
+$password = '';
+$dbname = 'world';
+$conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+$country = $_GET['country'];
+$all = $_GET['all'];
+if ($all == "true") {
+    $stmt = $conn->query("SELECT * FROM countries;");
+    
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+    echo '<ul>';
+    foreach ($results as $row) {
+        echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+    }
+    echo '</ul>';
+    
+}
+else if ($country !== ""){
+    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%';");
+    
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+    echo '<ul>';
+    foreach ($results as $row) {
+        echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+    }
+    echo '</ul>';
+}
